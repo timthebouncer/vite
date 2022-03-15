8c4509ed78e76c962a8240c0823906ef94f9e35b
timthebouncer <71633334+timthebouncer@users.noreply.github.com>
2022-03-15 23:47:31 +0800

    test

---
+++ houseRent/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import React from 'react'
+import Header from "@/pages/Header";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ houseRent/src/pages/Header/index.jsx
@@ -0,0 +1,16 @@
+import {useState} from "react";
+import React from 'react'
+
+
+const Header=()=>{
+
+    const [aaa,sss] = useState('s')
+
+    return(
+        <div>
+            6666
+        </div>
+    )
+}
+
+export default Header
